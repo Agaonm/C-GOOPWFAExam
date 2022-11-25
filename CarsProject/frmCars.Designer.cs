320dedbedaf09661f5db3a1e4011ae0ad10550c6
Agaonm <Conwaytima@gmail.com>
2022-11-25 17:25:51 +0000

    Add Files

---
+++ CarsProject/frmCars.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace CarsProject
+{
+    partial class frmCars
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbRegNo = new System.Windows.Forms.TextBox();
+            this.tbMake = new System.Windows.Forms.TextBox();
+            this.tbEngine = new System.Windows.Forms.TextBox();
+            this.tbDate = new System.Windows.Forms.TextBox();
+            this.tbRent = new System.Windows.Forms.TextBox();
+            this.cbAvailable = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbTotalEntries = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDlt = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bowman Car Hire";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 142);
+            this.label2.Margin = new System.Windows.Forms.Padding(15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vehicle registration number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 185);
+            this.label3.Margin = new System.Windows.Forms.Padding(15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Make";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 228);
+            this.label4.Margin = new System.Windows.Forms.Padding(15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Engine size";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 271);
+            this.label5.Margin = new System.Windows.Forms.Padding(15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Date registered";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 314);
+            this.label6.Margin = new System.Windows.Forms.Padding(15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Rental per day";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 357);
+            this.label7.Margin = new System.Windows.Forms.Padding(15);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Available";
+            // 
+            // tbRegNo
+            // 
+            this.tbRegNo.Location = new System.Drawing.Point(217, 139);
+            this.tbRegNo.Name = "tbRegNo";
+            this.tbRegNo.Size = new System.Drawing.Size(201, 20);
+            this.tbRegNo.TabIndex = 0;
+            // 
+            // tbMake
+            // 
+            this.tbMake.Location = new System.Drawing.Point(217, 182);
+            this.tbMake.Name = "tbMake";
+            this.tbMake.Size = new System.Drawing.Size(201, 20);
+            this.tbMake.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.tbMake, "Enter the make of the vehicle");
+            // 
+            // tbEngine
+            // 
+            this.tbEngine.Location = new System.Drawing.Point(217, 225);
+            this.tbEngine.Name = "tbEngine";
+            this.tbEngine.Size = new System.Drawing.Size(201, 20);
+            this.tbEngine.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.tbEngine, "Enter Vehicle Engine Size");
+            // 
+            // tbDate
+            // 
+            this.tbDate.Location = new System.Drawing.Point(217, 268);
+            this.tbDate.Name = "tbDate";
+            this.tbDate.Size = new System.Drawing.Size(201, 20);
+            this.tbDate.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.tbDate, "What Date was the Vehicle Registered on");
+            // 
+            // tbRent
+            // 
+            this.tbRent.Location = new System.Drawing.Point(217, 311);
+            this.tbRent.Name = "tbRent";
+            this.tbRent.Size = new System.Drawing.Size(201, 20);
+            this.tbRent.TabIndex = 4;
+            // 
+            // cbAvailable
+            // 
+            this.cbAvailable.AutoSize = true;
+            this.cbAvailable.Location = new System.Drawing.Point(217, 352);
+            this.cbAvailable.Name = "cbAvailable";
+            this.cbAvailable.Size = new System.Drawing.Size(15, 14);
+            this.cbAvailable.TabIndex = 5;
+            this.cbAvailable.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(27, 444);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 53);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "First";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(133, 444);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 53);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Previous";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbTotalEntries
+            // 
+            this.tbTotalEntries.Location = new System.Drawing.Point(239, 461);
+            this.tbTotalEntries.Name = "tbTotalEntries";
+            this.tbTotalEntries.Size = new System.Drawing.Size(100, 20);
+            this.tbTotalEntries.TabIndex = 99;
+            this.tbTotalEntries.TabStop = false;
+            this.tbTotalEntries.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(345, 444);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 53);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Next";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Location = new System.Drawing.Point(451, 444);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 53);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Last";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Transparent;
+            this.btnUpdate.Location = new System.Drawing.Point(555, 89);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 53);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "&Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdd.Location = new System.Drawing.Point(555, 149);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 53);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDlt
+            // 
+            this.btnDlt.BackColor = System.Drawing.Color.Transparent;
+            this.btnDlt.Location = new System.Drawing.Point(555, 208);
+            this.btnDlt.Name = "btnDlt";
+            this.btnDlt.Size = new System.Drawing.Size(100, 53);
+            this.btnDlt.TabIndex = 8;
+            this.btnDlt.Text = "&Delete";
+            this.btnDlt.UseVisualStyleBackColor = false;
+            this.btnDlt.Click += new System.EventHandler(this.btnDlt_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Transparent;
+            this.btnSearch.Location = new System.Drawing.Point(555, 267);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(100, 53);
+            this.btnSearch.TabIndex = 9;
+            this.btnSearch.Text = "&Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.Location = new System.Drawing.Point(555, 326);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 53);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExit.Location = new System.Drawing.Point(555, 385);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 53);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmCars
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(674, 509);
+            this.Controls.Add(this.tbTotalEntries);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDlt);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbAvailable);
+            this.Controls.Add(this.tbRent);
+            this.Controls.Add(this.tbDate);
+            this.Controls.Add(this.tbEngine);
+            this.Controls.Add(this.tbMake);
+            this.Controls.Add(this.tbRegNo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCars";
+            this.Text = "Task A - Tim Conway - 22/11/2022";
+            this.Load += new System.EventHandler(this.frmCars_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbRegNo;
+        private System.Windows.Forms.TextBox tbMake;
+        private System.Windows.Forms.TextBox tbEngine;
+        private System.Windows.Forms.TextBox tbDate;
+        private System.Windows.Forms.TextBox tbRent;
+        private System.Windows.Forms.CheckBox cbAvailable;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbTotalEntries;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDlt;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
---
+++ CarsProject/frmCars.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace CarsProject
+{
+    public partial class frmCars : Form
+    {
+        //Set up Database COnnection
+        OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Agaonm\Downloads\CarsDatabase.accdb");
+        // Set up Database Adapter
+        OleDbDataAdapter adapter;
+        //Set up Data Table 
+        DataTable dt = new DataTable();
+        //Set up Command for Running SQL Queries
+        OleDbCommand cmd;
+        //Start Database at row 0
+        int pos = 0;
+
+        public frmCars()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCars_Load(object sender, EventArgs e)
+        {
+            //Fill Form with Database Entries on load
+            string sql = "SELECT * FROM tblCars";
+            adapter = new OleDbDataAdapter(sql, conn);
+            adapter.Fill(dt);
+            showData(pos);
+        }
+
+
+        public void showData(int index)
+        {
+            //Set textboxs from Data Table
+            tbRegNo.Text = dt.Rows[index]["VehicleRegNo"].ToString();
+            tbMake.Text = dt.Rows[index]["Make"].ToString();
+            tbEngine.Text = dt.Rows[index]["EngineSize"].ToString();
+            //Convert Date and time to just Date
+            tbDate.Text = Convert.ToDateTime(dt.Rows[index]["DateRegistered"]).ToString("dd/MM/yyyy");
+            //Concert Number to Currency
+            tbRent.Text = "€" + Convert.ToDouble(dt.Rows[index]["RentalPerDay"]).ToString("#,##,0.00");
+           
+            //Check Available set checkbox to True
+            if (dt.Rows[index]["Available"].ToString() == "True"){
+                cbAvailable.Checked = true;
+            }
+            //Set Check box to False 
+            else
+            {
+                cbAvailable.Checked = false;
+            }
+
+            //Set the Bottom textbox to total number of entries
+            tbTotalEntries.Text = pos+1 + " of " + dt.Rows.Count;
+        }
+        //First Button - Shows first Entry
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pos = 0;
+            showData(pos);
+        }
+
+        //Previous Button, shows previous, Current - 1
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pos--;
+            if(pos >= 0)
+            {
+                showData(pos);
+            }
+            else
+            {
+                MessageBox.Show("END");
+            }
+        }
+
+        //Next Buton, Shows Next, Current + 1
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pos++;
+            if(pos < dt.Rows.Count)
+            {
+                showData(pos);
+            }
+            else
+            {
+                MessageBox.Show("END");
+                pos = dt.Rows.Count - 1;
+            }
+        }
+
+        //Last Button, Shows Last 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pos = dt.Rows.Count - 1;
+            showData(pos);
+        }
+
+        //Exit the Program
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Update Entry
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //Setup SQL Query
+            string sql = "UPDATE tblCars SET [Make] = ?, [EngineSize] = ?, [DateRegistered] = ?, [RentalPerDay] = ? WHERE [VehicleRegNo] = ? ";
+            //Start command
+            cmd = new OleDbCommand(sql, conn);
+            //Open Connection to Database
+            conn.Open();
+
+            //Sets paramters in [] to the variable next to it IN ORDER,
+            //e.g [EngineSize] = @EngineSize and tbEngine.Text = 2nd ?
+            //Must have parameters in the same order you put them in Query
+            cmd.Parameters.AddWithValue("@Make", tbMake.Text);
+            cmd.Parameters.AddWithValue("@EngineSize", tbEngine.Text);
+            cmd.Parameters.AddWithValue("@DateRegistered", tbDate.Text);
+            cmd.Parameters.AddWithValue("@RentalPerDay", tbRent.Text);
+            //cmd.Parameters.AddWithValue("@Available", cbAvailable.Text); ####HAVENT GOT THIS WORKING YET####
+            cmd.Parameters.AddWithValue("@VehicleRegNo", tbRegNo.Text);
+
+            //Run the Query
+            cmd.ExecuteNonQuery();
+
+            //Update the Program 
+            updateScreen();
+            //Close the Datbase connection
+            conn.Close();
+        }
+
+        //Add Entry
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //Set up SQL Query
+            string sql = "INSERT INTO tblCars ([VehicleRegNo],[Make],[EngineSize],[DateRegistered],[RentalPerDay]) VALUES (?,?,?,?,?)";
+            //Start Command
+            cmd = new OleDbCommand(sql, conn);
+            //Open Database Connection
+            conn.Open();
+
+            //Set parameters for Query, See Above for Explination
+            cmd.Parameters.AddWithValue("@VehicleRegNo", tbRegNo.Text);
+            cmd.Parameters.AddWithValue("@Make", tbMake.Text);
+            cmd.Parameters.AddWithValue("@EngineSize", tbEngine.Text);
+            cmd.Parameters.AddWithValue("@DateRegistered", tbDate.Text);
+            cmd.Parameters.AddWithValue("@RentalPerDay", tbRent.Text);
+            //cmd.Parameters.AddWithValue("@Available", cbAvailable.Text); ####Not working yet####
+            
+            //Run the Query
+            cmd.ExecuteNonQuery();
+            //Close connection
+            conn.Close();
+            //Update the program
+            updateScreen();
+        }
+        
+        //Delete an entry
+        private void btnDlt_Click(object sender, EventArgs e)
+        {
+            //Set up SQL Query
+            string sql = "DELETE FROM tblCars WHERE [VehicleRegNo] = ?";
+            //Start Command
+            cmd = new OleDbCommand(sql, conn);
+            //Open Database Connection
+            conn.Open();
+
+            //Set Parameter
+            cmd.Parameters.AddWithValue("@VehicleRegNo", tbRegNo.Text);
+            //Run Query
+            cmd.ExecuteNonQuery();
+            //Close Connection
+            conn.Close();
+            //Update Program
+            updateScreen();
+            //Goes back one - Not Needed!!
+            button4.PerformClick();
+        }
+
+        private void updateScreen()
+        {
+            dt.Reset();
+            adapter.Fill(dt);
+            this.Refresh();
+        }
+
+        //Open the Search Form
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            //Create search form
+            frmSearch frmSearch = new frmSearch();
+            //Show search Form
+            frmSearch.Show();
+        }
+    }
+}
